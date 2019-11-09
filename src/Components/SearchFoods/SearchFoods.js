f83801cf074d99b0874b3b3425ced2b80b0c6d8f
Miguel Ponce <miguelaponce258@gmail.com>
2019-11-08 17:26:22 -0700

    added error for empty search term

--- src/Components/SearchFoods/SearchFoods.js
+++ src/Components/SearchFoods/SearchFoods.js
@@ -1,71 +1,89 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import config from "../../config";
-import "./SearchFoods.css";
-import FoodItem from "../FoodItem/FoodItem";
-import AddFoodLogItem from "../AddFoodLogItem/AddFoodLogItem";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import config from '../../config';
+import './SearchFoods.css';
+import FoodItem from '../FoodItem/FoodItem';
+import AddFoodLogItem from '../AddFoodLogItem/AddFoodLogItem';
 
 export default class SearchBar extends Component {
   state = {
     error: null,
     searchResults: [],
     searchTerm: {
-      value: "",
+      value: '',
       touched: false
     }
   };
 
   handleSearch = ev => {
     ev.preventDefault();
-    const url =
-      config.FOOD_API_ENDPOINT +
-      `search?query=${this.state.searchTerm.value}&apiKey=${config.API_KEY}`;
-    fetch(url, {
-      method: "GET"
-    })
-      .then(res =>
-        !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-      )
-      .then(resJson => {
-        resJson.products.length >= 1
-          ? this.setState({ searchResults: resJson.products })
-          : this.setState({ error: "A food item by that name was not found" });
+    if (this.validateSearchTerm()) {
+      this.setState({ error: this.validateSearchTerm() });
+    } else {
+      const url =
+        config.FOOD_API_ENDPOINT +
+        `search?query=${this.state.searchTerm.value}&apiKey=${config.API_KEY}`;
+      fetch(url, {
+        method: 'GET'
       })
-      .catch(error => console.error(error));
+        .then(res =>
+          !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+        )
+        .then(resJson => {
+          resJson.products.length >= 1
+            ? this.setState({ searchResults: resJson.products })
+            : this.setState({
+                error: 'A food item by that name was not found'
+              });
+        })
+        .catch(error => console.error(error));
+    }
   };
 
   updateSearchTerm = term => {
     this.setState({ searchTerm: { value: term, touched: true } });
   };
 
+  validateSearchTerm = () => {
+    const searchTerm = this.state.searchTerm.value;
+    if (searchTerm.length < 1) {
+      return 'A search term is required';
+    }
+  };
+
   render() {
     const results = this.state.searchResults;
     return (
       <>
-        <form action='search' onSubmit={this.handleSearch}>
+        <form action="search" onSubmit={this.handleSearch}>
           <legend>
-            <h2 className='section-title'>Search for foods</h2>
+            <h2 className="section-title">Search for foods</h2>
           </legend>
-          <div className='food-search'>
+          <div className="food-search">
+            {this.state.error ? (
+              <p className="error">{this.state.error}</p>
+            ) : (
+              <></>
+            )}
             <input
-              type='text'
-              className='search-bar input'
+              type="text"
+              className="search-bar input"
               onChange={e => this.updateSearchTerm(e.target.value)}
-              placeholder='Food name'
+              placeholder="Food name"
             />
-            <button className='search-button button' type='submit'>
+            <button className="search-button button" type="submit">
               Search
             </button>
           </div>
         </form>
         {results.length >= 1 ? (
-          <div className='search-results'>
+          <div className="search-results">
             {results.map((food, i) => {
               return (
-                <div key={food.id} className='food-item-container'>
+                <div key={food.id} className="food-item-container">
                   {food.image !== undefined ? (
                     <img
-                      className='food-img'
+                      className="food-img"
                       src={food.image}
                       alt={food.name}
                     />
@@ -79,9 +97,7 @@ export default class SearchBar extends Component {
             })}
           </div>
         ) : (
-          <>
-            <p className='error'>{this.state.error}</p>
-          </>
+          <></>
         )}
       </>
     );
